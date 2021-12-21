85a6a1443a05d6f7a835171ac46029f0171295dd
yhrybniak <yhrybniak@drbsystems.com>
2021-12-21 19:54:35 +0200

    add monitor

---
+++ CasSynchronization/Monitor.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading;
+
+namespace CasSynchronization
+{
+    public static class Monitor
+    {
+        private static int _locked = 0;
+        private static Thread _lockThread;
+        private static bool _notify = false;
+        
+        public static void Lock()
+        {
+            while (Interlocked.CompareExchange(ref _locked, 1, 0) != 0)
+            {
+                Thread.Yield();
+            }
+            _lockThread = Thread.CurrentThread;
+        }
+
+        public static void Unlock()
+        {
+            _locked = 0;
+            _lockThread = null;
+        }
+
+        public static void Wait()
+        {
+            var currentThread = Thread.CurrentThread;
+            if (currentThread != _lockThread)
+            {
+                throw new SynchronizationLockException();
+            }
+
+            Unlock();
+            while (!_notify)
+            {
+                Thread.Yield();
+            }
+
+            Lock();
+            _notify = false;
+        }
+
+        public static void Notify()
+        {
+            var currentThread = Thread.CurrentThread;
+            if (currentThread != _lockThread)
+            {
+                throw new SynchronizationLockException();
+            }
+
+            _notify = true;
+        }
+    }
+}
---
+++ CasSynchronization/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading;
+
+namespace CasSynchronization
+{
+    class Program
+    {
+        private static int value = 0;
+        //unsynchronized
+        // static void Main(string[] args)
+        // {
+        //     var t1 = new Thread(() =>
+        //     {
+        //         for (int i = 0; i < 100; i++)
+        //         {
+        //             value++;
+        //             Console.WriteLine(value);
+        //         }
+        //     });
+        //     var t2 = new Thread(() =>
+        //     {
+        //         for (int i = 0; i < 100; i++)
+        //         {
+        //             value--;
+        //             Console.WriteLine(value);
+        //         }
+        //     });
+        //     t1.Start();
+        //     t2.Start();
+        //     t1.Join();
+        //     t2.Join();
+        // }
+        //
+        //synchronized
+        static void Main(string[] args)
+        {
+            var t1 = new Thread(() =>
+            {
+                Monitor.Lock();
+                for (int i = 0; i < 100; i++)
+                {
+                    value++;
+                    Console.WriteLine(value);
+                }
+                Monitor.Unlock();
+            });
+            var t2 = new Thread(() =>
+            {
+                Monitor.Lock();
+                for (int i = 0; i < 100; i++)
+                {
+                    value--;
+                    Console.WriteLine(value);
+                }
+                Monitor.Unlock();
+            });
+            t1.Start();
+            t2.Start();
+            t1.Join();
+            t2.Join();
+        }
+    }
+}
